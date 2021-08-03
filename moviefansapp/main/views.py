a76a9214bb4968eea6b6e85cb1cd5fe17486fc6b
Grant Woollon <2264109w@student.gla.ac.uk>
2021-08-03 15:30:05 +0100

    initial setup of index.html, confirmed working with extending base.html template; removed unneeded template tags from base.html

--- moviefansapp/main/views.py
+++ moviefansapp/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Testing new app')
+    return render(request, 'main/index.html',)
