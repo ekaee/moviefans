b6a27e532c78ba1b1735af53fe22ada8ffc32ecd
weihang you <2587213y@studdent.gla.ac.uk>
2021-08-05 02:43:35 +0100

    Add test file

--- moviefansapp/main/tests.py
+++ moviefansapp/main/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from models import Movie
 
 # Create your tests here.
+
+
+class CinemaPagesTestCase(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_login(self):
+        response = self.client.get('/login/')
+        self.failUnlessEqual(response.status_code, 200)
+        
+    def test_register(self):
+        response = self.client.get('/register/')
+        self.failUnlessEqual(response.status_code, 200)
+        
+    def test_about(self):
+        response = self.client.get('/about/')
+        self.failUnlessEqual(response.status_code, 200)
+        
+    def test_movie(self):
+        response = self.client.get('/movie/action-movie-1')
+        self.failUnlessEqual(response.status_code, 200)
+    
+    def test_search(self):
+        response = self.client.post('/search/', {'q': 'big'})
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_genre(self):
+        response = self.client.get('/genre/action')
+        self.assertIn(response.status_code, (301, 302))
+
