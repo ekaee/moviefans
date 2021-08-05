3d3d5ec07a3907636a75d0e0336de26622e44eb9
jerrykress <jerrykress.wsj@gmail.com>
2021-08-05 11:59:58 +0100

    Register Models in Admin

--- moviefansapp/main/admin.py
+++ moviefansapp/main/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from main.models import UserProfile, Movie, Genre, Comments
 
 # Register your models here.
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Movie, MovieAdmin)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Genre, GenreAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Comments, CommentAdmin)
